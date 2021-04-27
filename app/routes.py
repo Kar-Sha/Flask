8f07f156e17e375af7897aa2a6a845b6c7d29c80
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2021-04-26 23:05:52 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,30 @@ def home():
     if form.validate_on_submit():
         # check if user exits in database
         # if not create user and add to database
-        # create row in Message table with user (created/found) add to ta database
-
-    posts = []
+        user = User.query.filterby(author = form.author.data)
+        
+        if user is None:
+            newUser = User(author = form.author.data)
+            db.session.add(newUser)
+        # create row in Message table with user (created/found) add to the database
+        messageRow = Messages(message = form.message.data, user_id = user.query.filterby(author = form.author.data).first().id)
+        db.session.add(messageRow)
+        db.session.commit()
+        
+   
     # output all messages
     # create a list of dictionaries with the following structure
-    # [{'author':'carlos', 'message':'Yo! Where you at?!'},
-    #  {'author':'Jerry', 'message':'Home. You?'}]
+    posts = 
+    [{'author':'carlos', 'message':'Yo! Where you at?!'},
+    {'author':'Jerry', 'message':'Home. You?'}]
+    
+    dictList = Messages.query.all()
+    for i in someList:
+        posts = post + [
+            {
+                'author':' {}'.format(User.query.filter_by(id = i.user_id).first().author),
+                'message':' {}'.format(i.message)
+            }]
 
     return render_template('home.html', posts=posts, form=form)
 
